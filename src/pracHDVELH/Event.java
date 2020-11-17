0508d0841297ef6c03fb48b07398f8d5759320c1
Jean-Christophe-Nguyen <jean-christophe.nguyen@etu.univ-amu.fr>
2020-11-17 15:17:36 +0100

    Le programme marche mais encore rien de fait

--- src/pracHDVELH/Event.java
+++ src/pracHDVELH/Event.java
@@ -18,105 +18,108 @@ public class Event extends NodeMultiple {
 	public static final String PROMPT_ANSWER = "Answer: ";
 	public static final String WARNING_MSG_INTEGER_EXPECTED = "Please input a integer within range!";
 
-	/**
-	 * @return the playerAnswer
-	 */
-	public String getPlayerAnswer() {
-		/* TO BE COMPLETED */
-	}
+	public Event(GUIManager gui, String text){
 
-	/**
-	 * @param playerAnswer the playerAnswer to set
-	 */
-	public void setPlayerAnswer(String playerAnswer) {
-		/* TO BE COMPLETED */
 	}
-
-	/**
-	 * @return the reader
-	 */
-	public Scanner getReader() {
-		/* TO BE COMPLETED */
-	}
-
-	/**
-	 * @param reader the reader to set
-	 */
-	public void setReader(Scanner reader) {
-		/* TO BE COMPLETED */
-	}
-
-	/**
-	 * @return the chosenPath
-	 */
-	public int getChosenPath() {
-		/* TO BE COMPLETED */
-	}
-
-	/**
-	 * @param chosenPath the chosenPath to set
-	 */
-	public void setChosenPath(int chosenPath) {
-		/* TO BE COMPLETED */
-	}
-
-	/* Methods */
-	/**
-	 * @see pracHDVELH.NodeMultiple#getData()
-	 */
-	public String getData() {
-		/* TO BE COMPLETED */
-	}
-
-	/**
-	 * @see pracHDVELH.NodeMultiple#setData(Object)
-	 * @param data
-	 */
-	public void setData(String data) {
-		/* TO BE COMPLETED */
-	}
-
-	/**
-	 * @see pracHDVELH.NodeMultiple#getDaughter(int)
-	 */
-	@Override
-	public Event getDaughter(int i) {
-		/* TO BE COMPLETED */
-	}
-
-	/**
-	 * @see pracHDVELH.NodeMultiple#setDaughter(NodeMultiple, int)
-	 * @param daughter
-	 * @param i
-	 */
-	public void setDaughter(Event daughter, int i) {
-		/* TO BE COMPLETED */
-	}
-
-	/**
-	 * @return the gui
-	 */
-	public GUIManager getGui() {
-		/* TO BE COMPLETED */
-	}
-
-	/**
-	 * @param gui the gui to set
-	 */
-	public void setGui(GUIManager gui) {
-		/* TO BE COMPLETED */
-	}
-
-	/**
-	 * @return the id
-	 */
-	public int getId() {
-		/* TO BE COMPLETED */
-	}
-
-	/* Methods */
-	/* TO BE COMPLETED */
-	}
+//	/**
+//	 * @return the playerAnswer
+//	 */
+//	public String getPlayerAnswer() {
+//		/* TO BE COMPLETED */
+//	}
+//
+//	/**
+//	 * @param playerAnswer the playerAnswer to set
+//	 */
+//	public void setPlayerAnswer(String playerAnswer) {
+//		/* TO BE COMPLETED */
+//	}
+//
+//	/**
+//	 * @return the reader
+//	 */
+//	public Scanner getReader() {
+//		/* TO BE COMPLETED */
+//	}
+//
+//	/**
+//	 * @param reader the reader to set
+//	 */
+//	public void setReader(Scanner reader) {
+//		/* TO BE COMPLETED */
+//	}
+//
+//	/**
+//	 * @return the chosenPath
+//	 */
+//	public int getChosenPath() {
+//		/* TO BE COMPLETED */
+//	}
+//
+//	/**
+//	 * @param chosenPath the chosenPath to set
+//	 */
+//	public void setChosenPath(int chosenPath) {
+//		/* TO BE COMPLETED */
+//	}
+//
+//	/* Methods */
+//	/**
+//	 * @see pracHDVELH.NodeMultiple#getData()
+//	 */
+//	public String getData() {
+//		/* TO BE COMPLETED */
+//	}
+//
+//	/**
+//	 * @see pracHDVELH.NodeMultiple#setData(Object)
+//	 * @param data
+//	 */
+//	public void setData(String data) {
+//		/* TO BE COMPLETED */
+//	}
+//
+//	/**
+//	 * @see pracHDVELH.NodeMultiple#getDaughter(int)
+//	 */
+//	@Override
+//	public Event getDaughter(int i) {
+//		/* TO BE COMPLETED */
+//	}
+//
+//	/**
+//	 * @see pracHDVELH.NodeMultiple#setDaughter(NodeMultiple, int)
+//	 * @param daughter
+//	 * @param i
+//	 */
+//	public void setDaughter(Event daughter, int i) {
+//		/* TO BE COMPLETED */
+//	}
+//
+//	/**
+//	 * @return the gui
+//	 */
+//	public GUIManager getGui() {
+//		/* TO BE COMPLETED */
+//	}
+//
+//	/**
+//	 * @param gui the gui to set
+//	 */
+//	public void setGui(GUIManager gui) {
+//		/* TO BE COMPLETED */
+//	}
+//
+//	/**
+//	 * @return the id
+//	 */
+//	public int getId() {
+//		/* TO BE COMPLETED */
+//	}
+//
+//	/* Methods */
+//	/* TO BE COMPLETED */
+//	}
 }
 
 // eof
--- src/pracHDVELH/Scenario.java
+++ src/pracHDVELH/Scenario.java
@@ -10,72 +10,82 @@ package pracHDVELH;
  *
  */
 public class Scenario {
-	private static final String MSG_EMPTY_SCENARIO = "Sorry, no scenario was found.";
-	private static final String MSG_FINALE = "That's all folks!";
-	private Event head;
-	private GUIManager gui;
+    private static final String MSG_EMPTY_SCENARIO = "Sorry, no scenario was found.";
+    private static final String MSG_FINALE = "That's all folks!";
+    private Event head;
+    private GUIManager gui;
 
-	/* TO BE COMPLETED */
+    /* TO BE COMPLETED */
+    public Scenario(GUIManager gui, Event head){
+    this.gui = gui;
+    this.head = head;
+    }
 
-	/* MAIN */
-	public static void main(String[] args) {
-		Scenario scenario;
-		GUIManager gui = new GUIManager(System.in, System.out, System.err);
+    public String run (){
+        return "test";
+    }
 
-		// S
-		// *1:event1
-		// **1.1
-		// ***S
-		// **1.2
-		// ***E
-		// *2:event2
-		// **2.1
-		// ***1
-		// **2.2
-		// ***S
+    /* MAIN */
+    public static void main(String[] args) {
+        Scenario scenario;
+        GUIManager gui = new GUIManager(System.in, System.out, System.err);
 
-		Event startEvent = new Event(gui, "Go!\n" + "(1)1 (2)2");
-		Event event1 = new Event(gui, "event1:\n" + "(1)1.1 (2)1.2");
-		Event event2 = new Event(gui, "event2:\n" + "(1)2.1 (2)2.2");
-		Event endEvent = new Event(gui, "End event: that's it :-)");
-		startEvent.addDaughter(event1);
-		startEvent.setDaughter(event2, 1);
-		event1.addDaughter(startEvent);
-		event1.addDaughter(endEvent);
-		event2.addDaughter(event1);
-		event2.addDaughter(startEvent);
-		scenario = new Scenario(gui, startEvent);
 
-		// *2
-		// ...
-		// **2.3:event3
-		// ***E
-		// ***event3
+        // S
+        // *1:event1
+        // **1.1
+        // ***S
+        // **1.2
+        // ***E
+        // *2:event2
+        // **2.1
+        // ***1
+        // **2.2
+        // ***S
 
-		Event event3 = new EventExactSolution(gui, "Wizard: how much is worth pi?", "3.14159");
-		event2.setData(event2.getData() + " (3)2.3");
-		event2.addDaughter(event3);
-		event3.addDaughter(endEvent);
-		event3.addDaughter(event3);
+        Event startEvent = new Event(gui, "Go!\n" + "(1)1 (2)2");
+        Event event1 = new Event(gui, "event1:\n" + "(1)1.1 (2)1.2");
+        Event event2 = new Event(gui, "event2:\n" + "(1)2.1 (2)2.2");
+        Event endEvent = new Event(gui, "End event: that's it :-)");
+        startEvent.addDaughter(event1);
+        startEvent.setDaughter(event2, 1);
+        event1.addDaughter(startEvent);
+        event1.addDaughter(endEvent);
+        event2.addDaughter(event1);
+        event2.addDaughter(startEvent);
+        scenario = new Scenario(gui, startEvent);
 
-		/* ******* */
-		// **2.3
-		// ***event4
-		// ****event2
-		// ****E
-		// ****event3
-		// ...
+        // *2
+        // ...
+        // **2.3:event3
+        // ***E
+        // ***event3
 
-		int[] mask = { 3, 6, 7 };
-		Event event4 = new EventRandomSolution(gui, "Random choice of the next event...", mask, "Dice rolling... Roll=",
-				"\nNext event is ");
-		event3.setDaughter(event4, 0);
-		event4.addDaughter(event2);
-		event4.addDaughter(endEvent);
-		event4.addDaughter(event3);
+//		Event event3 = new EventExactSolution(gui, "Wizard: how much is worth pi?", "3.14159");
+//		event2.setData(event2.getData() + " (3)2.3");
+//		event2.addDaughter(event3);
+//		event3.addDaughter(endEvent);
+//		event3.addDaughter(event3);
 
-		System.out.println(scenario.run());
-	}
+        /* ******* */
+        // **2.3
+        // ***event4
+        // ****event2
+        // ****E
+        // ****event3
+        // ...
+
+//		int[] mask = { 3, 6, 7 };
+//		Event event4 = new EventRandomSolution(gui, "Random choice of the next event...", mask, "Dice rolling... Roll=",
+//				"\nNext event is ");
+//		event3.setDaughter(event4, 0);
+//		event4.addDaughter(event2);
+//		event4.addDaughter(endEvent);
+//		event4.addDaughter(event3);
+
+        System.out.println(scenario.run());
+
+    }
 }
 
 // eof
